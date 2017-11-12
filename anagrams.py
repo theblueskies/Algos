3b03af469142924ac364820a0de3035080e68e01
Blade <druhin.bala@gmail.com>
2017-11-12 09:19:47 -0800

    updates

--- anagrams.py
+++ anagrams.py
@@ -16,15 +16,15 @@ def anagram(word):
 # anagram('car')
 
 def anagrams(word):
-    """ Generate all of the anagrams of a word. """ 
+    """ Generate all of the anagrams of a word. """
     if len(word) < 2:
         yield word
     else:
         for i, letter in enumerate(word):
             # if not letter in word[:i]: #avoid duplicating earlier words
                 for j in anagrams(word[:i]+word[i+1:]):
-                    yield j+letter 
+                    yield j+letter
 
 if __name__ == "__main__":
-    for i in anagram("car"):
-        pass
+    for i in anagrams("car"):
+        print i
--- permutation.py
+++ permutation.py
@@ -1,9 +1,9 @@
 # Python program to print all permutations with
 # duplicates allowed
- 
+
 def toString(List):
     return ''.join(List)
- 
+
 # Function to print permutations of string
 # This function takes three parameters:
 # 1. String
@@ -11,16 +11,14 @@ def toString(List):
 # 3. Ending index of the string.
 def permute(a, l, r):
     if l==r:
-        print toString(a)
+        print (toString(a))
     else:
         for i in range(l,r+1):
-            print ('l = ', l, ' r= ', r)
             a[l], a[i] = a[i], a[l]
-            # print ('before backtrack ', a)
             permute(a, l+1, r)
             a[l], a[i] = a[i], a[l] # backtrack
 
-# Driver program to test the above function
+
 string = "ABC"
 n = len(string)
 a = list(string)
